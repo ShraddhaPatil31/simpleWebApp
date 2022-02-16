446bb46bc509d97397e96e2737853988abd461de
ShraddhaPatil31 <99398725+ShraddhaPatil31@users.noreply.github.com>
2022-02-16 17:53:57 +0530

    Added files
    files are added at begining

---
+++ Q1_calc.js
@@ -0,0 +1,18 @@
+exports.add=function(a,b){  //exports---> to make function globaly available
+    return a+b;
+}
+exports.subtract=function(a,b){
+    return a-b;
+}
+exports.multiply=function(a,b){
+    return a*b;
+}
+exports.divide=function(a,b){
+    return a/b;
+}
+exports.square=function(a){
+    return a*a;
+}
+exports.sum=function(a,b,c){
+    
+}
---
+++ Q3_Circle.js
@@ -0,0 +1,8 @@
+exports.calcArea=function(r){
+    var area=Math.PI*r*r;
+    return area;
+}
+exports.calcCircumference=function(r){
+    var circum=2*Math.PI*r;
+    return circum;
+}
